b5e5f394c0ca709f8f94aeb0f3f1a37766efa6cd
kong0107 <kong0107@gmail.com>
2019-01-26 00:36:42 +0800

    Updates

---
+++ precache-manifest.3b1d619fae4a0c2496f57d31e07938f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22b3fd7dbe636ca2a81e",
+    "url": "/lis.ly/static/js/runtime~main.22b3fd7d.js"
+  },
+  {
+    "revision": "2a67dfa2298e2e87f4bd",
+    "url": "/lis.ly/static/js/main.2a67dfa2.chunk.js"
+  },
+  {
+    "revision": "3319466e279795843f99",
+    "url": "/lis.ly/static/js/1.3319466e.chunk.js"
+  },
+  {
+    "revision": "2a67dfa2298e2e87f4bd",
+    "url": "/lis.ly/static/css/main.9dd68ecd.chunk.css"
+  },
+  {
+    "revision": "f286b9376427338af684e7745e27240e",
+    "url": "/lis.ly/index.html"
+  }
+];
